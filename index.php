921b78d7684b66b0769aece04a99bfc49423028b
romeo <claudio.romeo@laplateforme.io>
2021-11-24 12:48:35 +0100

    transfert de pc

---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="Fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css"  />
+
+    <title>Document</title>
+</head>
+<body>
+
+<header>
+    <nav>
+<ul>
+    <li class="tete"><a href="#">Accueil</a></li>
+    </ul>
+
+  <ul class="listindex">
+  <li><a href="connexion.php">connexion</a></li>
+    <li><a href="inscription.php">inscription</a></li>
+    <li><a href="profil.php">profil</a></li>
+  </ul>
+   </nav>
+
+
+</header>
+
+
+<main>
+
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium dicta placeat facilis in saepe omnis libero reprehenderit repellat nemo dolores, amet reiciendis voluptates at magnam nobis consequatur molestiae quae, dolorum soluta! Officia sed exercitationem ratione distinctio, laboriosam libero harum similique corporis, voluptatem molestias, velit amet ad a vitae eum nesciunt.
+</p>
+</main>
+
+
+</body>
+</html>
---
+++ inscription.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css" />
+
+    <title>Document</title>
+</head>
+<header>
+<ul>
+    <li><a href="index.php">Accueil</a></li>
+    <li><a href="inscription.php">inscription</a></li>
+    <li><a href="connexion.php">connexion</a></li>
+    <li><a href="profil.php">profil</a></li>
+  
+  </ul>
+</header>
+<body>
+    <p>Je suis la page d'inscription</p>
+</body>
+</html>
