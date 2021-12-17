abb47ff8dcb3d7c4f91c97fa10b85aee91eb0548
romeo claudio <claudio.romeo@laplateforme.io>
2021-12-17 17:15:26 +0100

    rajout du logo git

--- footer.php
+++ footer.php
@@ -9,6 +9,9 @@
   <a href="http://www.instagram.com/baka_le_marsien" target="_blank"><i class="fa fa-instagram"></i></a>
   <a href="https://facebook.com/claudio137" target="_blank"><i class="fa fa-facebook"></i></a>
   <a href="https://www.instagram.com/baka_le_marsien/" target="_blank"><i class="fa fa-twitch"></i></a>
+  <div>
+                            <a href="https://github.com/claudio-romeo/moduledeconnexion"><img class="imagegit" src="https://th.bing.com/th/id/R.734888c84d95d28b36728ac33186cab3?rik=EyUQGBjtSbMjVw&riu=http%3a%2f%2fpngimg.com%2fuploads%2fgithub%2fgithub_PNG80.png&ehk=sCQlSHnb7Wc8WNPgOilokXbf8jL4g20yv7QFEFpl6ko%3d&risl=&pid=ImgRaw&r=0" height="100" alt="Instagram"></a>
+                        </div>
  
 
 
