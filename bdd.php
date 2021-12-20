18247d04ec137863fa0bb9c7262d9217b0cead3b
romeo claudio <claudio.romeo@laplateforme.io>
2021-12-20 03:56:34 +0100

    alors c'est peut etre comme ca ?

--- bdd.php
+++ bdd.php
@@ -1,7 +1,7 @@
 
 <?php 
 session_start();
-$bdd = mysqli_connect('localhost' , 'root','','claudio-romeo-moduleconnexion');
+$bdd = mysqli_connect('localhost' , 'Deku','motdepass','claudio-romeo-moduleconnexion');
 mysqli_set_charset($bdd, 'utf8');
 ?>
 
